18f8aed69b48f2e47e8a7a9edc6e8a0a661921ac
Aiden Watson <awats1819@gmail.com>
2020-05-26 12:58:52 -0600

    encryption echo test working

--- server_py.py
+++ server_py.py
@@ -2,6 +2,20 @@ import socket
 import sys
 import select
 import upload
+from encryption import encrypter, decrypter, init_key, init_iv
+
+
+def ping():
+    first = "ping\n"
+    first = encrypter(first)
+    connection.sendall(first)
+    while True:
+        line = sys.stdin.readline()
+        encrypted = encrypter(line)
+        connection.sendall(encrypted)
+        recved = connection.recv(128).strip(b'\00')
+        decryp = decrypter(recved)
+        print(decryp)
 
 
 def shell():
@@ -14,42 +28,49 @@ def shell():
             # checks if value is available for recv
             r, _, _ = select.select([connection], [], [], 0)
             if r:
-                data = connection.recv(2048)
+                data = connection.recv(128)
                 if not data: break  # breaks from while True loop
                 print(data.decode('UTF-8'))
 
 
-# socket initialize
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("socket created")
+if __name__ == '__main__':
+    # socket initialize
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("socket created")
 
-# reserving port 4444 for incoming connections
-port = 4444
+    # reserving port 4444 for incoming connections
+    port = 4444
 
-s.bind(('', port))
-s.listen(1)
-print('server started waiting for connections')
+    s.bind(('', port))
+    s.listen(1)
+    print('server started waiting for connections')
 
-while True:
-    # waiting for connection
-    connection, client_addr = s.accept()
-    try:
-        print("connection from", client_addr[0])
-        while 1:
-            # non blocking input read
-            msg = sys.stdin.readline()
-            if msg:
-                if msg.find('upload') == 0:
-                    upload.send(msg[7:].replace('\n', ''), connection)
-                elif msg == 'close\n':
-                    connection.sendall('conend'.encode('UTF-8'))
-                    connection.close()
-                    exit()
-                elif msg.find('download') == 0:
-                    upload.download(msg[9:].replace('\n', ''), connection)
-                elif msg == 'shell\n':
-                    shell()
-    except KeyboardInterrupt:
-        s.close()
-        connection.close()
-        exit()
+    # initailize key values
+    init_key()
+    init_iv()
+
+    while True:
+        # waiting for connection
+        connection, client_addr = s.accept()
+        try:
+            print("connection from", client_addr[0])
+            while 1:
+                # non blocking input read
+                msg = sys.stdin.readline()
+                if msg:
+                    if msg.find('upload') == 0:
+                        upload.send(msg[7:].replace('\n', ''), connection)
+                    elif msg == 'close\n':
+                        connection.sendall('conend'.encode('UTF-8'))
+                        connection.close()
+                        exit()
+                    elif msg.find('download') == 0:
+                        upload.download(msg[9:].replace('\n', ''), connection)
+                    elif msg == 'shell\n':
+                        shell()
+                    elif msg == 'ping\n':  # only for testing
+                        ping()
+        except KeyboardInterrupt:
+            s.close()
+            connection.close()
+            exit()
--- upload.py
+++ upload.py
@@ -1,26 +1,27 @@
 import os
 from _socket import MSG_WAITALL
+from encryption import decrypter, encrypter
 
-DEFAULT_BUFF = 2048  # goto buffer size
+DEFAULT_BUFF = 128  # goto buffer size
 
 
 def send(file_name, connection):  # send files to client
-    connection.sendall('send\n'.encode('UTF-8'))  # alert client
+    connection.sendall(encrypter('send\n'.encode('UTF-8')))  # alert client
     print('sent')
     i = file_name.rfind('/')  # find index of last / in file destination
-    connection.sendall(file_name[i + 1:].encode('UTF-8'))  # send filename without the destination
+    connection.sendall(encrypter(file_name[i + 1:].encode('UTF-8')))  # send filename without the destination
     connection.recv(1)  # wait for ack
 
     totalbytes = os.path.getsize(file_name)
-    connection.sendall(str(totalbytes).encode('UTF-8'))
-    response = connection.recv(1).decode('UTF-8')  # wait for ack
+    connection.sendall(encrypter(totalbytes.encode('UTF-8')))
+    response = decrypter(connection.recv(1)).decode('UTF-8') # wait for ack
 
     if response == 'K':
         file = open(file_name, 'rb')  # opens file and reads it in byte form
-        buffer = file.read(2048)  # read 2048 bytes from file to buffer
+        buffer = file.read(DEFAULT_BUFF)  # read BUF LEN bytes from file to buffer
         while buffer:
             connection.sendall(buffer)
-            buffer = file.read(2048)  # read next 2048 bytes
+            buffer = file.read(DEFAULT_BUFF)  # read next BUF LEN bytes
         file.close()
     response = connection.recv(1).decode('UTF-8')
     if response == 'K':
@@ -33,18 +34,18 @@ def download(file_path, connection):  # upload files from client to server
 
     connection.sendall('download\n'.encode('UTF-8'))
 
-    connection.sendall(file_path.encode('UTF-8'))  # alert client to wanted file
-    response = connection.recv(1).decode('UTF-8')  # wait for client ack
+    connection.sendall(encrypter(file_path.encode('UTF-8')))  # alert client to wanted file
+    response = decrypter(connection.recv(1)).decode('UTF-8')  # wait for client ack
 
     # correct ack if file found
     if response == 'K':
-        str_size = connection.recv(10).decode('UTF-8')  # file size sent by client
+        str_size = decrypter(connection.recv(16)).decode('UTF-8')  # file size sent by client
         # remove any extra /0 from C send value and turns to int
         size = int(str_size[:str_size.find('\x00')])
 
         print('file size: ', size)
 
-        connection.sendall('K'.encode('UTF-8'))
+        connection.sendall('K'.encode('UTF-8'))  # no need to encrypt
         file = open(file_name, 'wb')  # open file in write binary mode
 
         amt_recv = 0
@@ -55,7 +56,7 @@ def download(file_path, connection):  # upload files from client to server
                 file.write(tmpbuff)
                 break
             buffer = connection.recv(DEFAULT_BUFF, MSG_WAITALL)
-            amt_recv += DEFAULT_BUFF  # increment by 2048/buffer size
+            amt_recv += DEFAULT_BUFF  # increment by BUF LEN/buffer size
             file.write(buffer)
         file.close()
         print("fill successfully downloaded")
