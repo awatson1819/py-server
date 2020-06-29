f23c35fa48643ed8c093c3222f7a61f247f44ab3
Aiden Watson <awats1819@gmail.com>
2020-06-29 16:27:37 -0600

    changed download to work with new threading method in client

--- upload.py
+++ upload.py
@@ -1,5 +1,5 @@
 import os
-from _socket import MSG_WAITALL
+from socket import MSG_WAITALL
 from encryption import decrypter, encrypter
 
 DEFAULT_BUFF = 128  # goto buffer size
@@ -34,36 +34,17 @@ def send(file_name, connection):  # send files to client
 def download(file_path, connection):  # upload files from client to server
     #  figures out file name in windows path
     file_name = (file_path[file_path.rfind('\\') + 1:])
+    file = open(file_name, 'wb')
+    while True:
+        size = connection.recv(4, MSG_WAITALL).strip(b'\00').decode()  # client sends size
+        size = int(size)  # convert sent str to proper int
+        #  wait for specified amount of data
+        data = decrypter(connection.recv(size, MSG_WAITALL))
+        file.write(data)  # write recv data to file
 
-    # notify client to command
-    connection.sendall(encrypter('download\n'.encode()))
+        # once data of max length is sent it means file end has been reached
+        if len(data) < MAX_READ:
+            break
+    file.close()
+        
 
-    connection.sendall(encrypter(file_path.encode('UTF-8')))  # alert client to wanted file
-    response = connection.recv(1).decode('UTF-8')  # wait for client ack
-
-    # correct ack if file found
-    if response == 'K':
-        # strip extra empty bytes and decode
-        str_size = decrypter(connection.recv(16).strip(b'\00')).decode()  # file size sent by client
-        # remove any extra /0 from C send value and turns to int
-        size = int(str_size)
-
-        # for troubleshooting
-        print('file size: ', size)
-
-        connection.sendall('K'.encode('UTF-8'))  # no need to encrypt
-
-        file = open(file_name, 'wb')  # open file in write binary mode
-
-        amt_recv = 0
-        while amt_recv < size:
-            # recv message and strip any extra \00's of unused space
-            buffer = connection.recv(DEFAULT_BUFF)
-
-            decrypted = decrypter(buffer)
-
-            # increment amount recv by size of plaintext
-            amt_recv += len(decrypted)
-            file.write(decrypted)
-        file.close()
-        print("fill successfully downloaded")
